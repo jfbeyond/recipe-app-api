fd8c9e9325e49878f1d520e2478b7f4ab3e7f044
Jhon Diaz <jhondiaz@jhons-mbp.myfiosgateway.com>
2023-04-23 12:57:01 -0400

    Correct linting issues for setting up db model

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,3 @@
-from django.db import models  # noqa
-
 """
 Dtabase models.
 """
@@ -16,11 +14,23 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
