4128b96657ebd4148219f45bd34770b9c3bab084
Jhon Diaz <jhondiaz@jhons-mbp.myfiosgateway.com>
2023-04-28 11:57:41 -0400

    Implement recipe detail API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,3 +13,11 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+
